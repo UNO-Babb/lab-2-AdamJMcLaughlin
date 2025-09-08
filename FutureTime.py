80e9a7a1a2abfe09cde86b4016b6e98a1cb370ae
AdamJMcLaughlin <77008370@nebraska.edu>
2025-09-08 03:10:21 +0000

    All done with Lab2

--- FutureTime.py
+++ FutureTime.py
@@ -3,13 +3,15 @@
 #Date:
 #Assignment:
 
+print("Adam McLaughlin\n09/04/2025\nFutureTime")
+print()
 # datetime will allow us to access the system date and time.
 import datetime
 
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 5) % 24
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
@@ -17,13 +19,26 @@ def main():
   #TODO:
   #Ask user for hours
   #Ask user for minutes
+  startHour = int(input("Enter starting hour (0-23)"))
+  startMinute = int(input("Enter starting minute (0-59)"))
+  moreMins = 500
 
+  totalMinutes = startMinute + moreMins
+  futureMins = (currentMinute + moreMins ) % 60
+  extraHour = (currentMinute + moreMins ) // 60
+  futureHour = (startHour + extraHour) % 24
+
+  print(extraHour)
+
+  print(futureMins)
   #Calculate the time after the user-supplied time has passed.
+ 
 
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
-
+  print(f"{futureHour:02d}:{futureMins:02d}")
 
 if __name__ == '__main__':
   main()
+
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,29 @@
-#Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
-
+print("Adam McLaughlin\n09/04/2025\nMagic8Ball")
+print()
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
+
   print("Magic 8 Ball")
+
+question = input("Ask the Magic 8 Ball a yes-or-no question: ")
+print("\nThinking...\n")
+
   #Prompt the user for their question.
 
+answers =["Hell No!","Hell Yeah!","Wish In One Hand And Shit In The Other.",
+"Does A Bear Shit In The Woods","I Have A Headache","Don't Quit Your      Day Job",
+"Please.","You Never Step In The Same River Twice","You Don't Find The Kundalini The Kundalini Finds You!",
+"Dude...","Dude!","Ask Your Mom!","Ask Your Dad!","The Universe Approves",
+"You Will Get Worms!!!","What Are You Asking Me!","Your Funny, Really!"]
+
   #Answer question randomly with one of the options from your earlier list.
-
-
-if __name__ == '__main__':
-  main()
+length = len(answers)
+r = random.random() * length
+r = int(r)
+ 
+print (r)
+response = answers[r]
+print (response)
